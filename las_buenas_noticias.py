04b2b1dab9a922f1d3548394347562272db15742
Samir Rojas <samirrojas162@gmail.com>
2020-04-25 15:10:33 -0430

    user input for dinamic weather search

--- las_buenas_noticias.py
+++ las_buenas_noticias.py
@@ -14,15 +14,18 @@ FEEDS = {'bbc':"http://feeds.bbci.co.uk/news/rss.xml",
         'fox': 'http://feeds.foxnews.com/foxnews/latest',
         'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London, UK'}
+
 
 def get_weather(query):
     # go to this URL to see the used JSON format, cambia q={} por q=London,uk
-    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=52eae8bbb172a9887578373c388e145f"
+    weather_api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=52eae8bbb172a9887578373c388e145f"
     # we make the query part configurable, so the City to retrieve data from is Dynamic
     print('\nquery---\n', query)
     # urllib.parse.quote elimina espacios en blanco; al usuario ingresar nombres de ciudad, le puede poner espacio
     query = urllib.parse.quote(query)
-    url = api_url.format(query)
+    url = weather_api_url.format(query)
     # we retrieve the JSON data stored in the taret page
     data = urllib.request.urlopen(url).read()
     print('\nurl http json data ---\n', data)
@@ -33,35 +36,39 @@ def get_weather(query):
         # this calls the parsed JSON data
         weather = {"description": parsed["weather"][0]["description"],
                    "temperature": parsed["main"]["temp"],
-                   "city": parsed["name"]
-                   }
+                   "city": parsed["name"],
+                   "country": parsed['sys']['country']
+                   } # the API response has the country code listed under 'sys'
     return weather
 
 
-@app.route("/")
-def get_noticas():
-    # request.args grabs get requests, request.form grabs POST requests
-    query = request.args.get("publication") # the tag name given in the html form
+def get_noticas(query):
     if not query or query.lower() not in FEEDS:
-        publication = "bbc"
+        publication = DEFAULTS['publication']
     else:
         publication = query.lower()
-
-    # EXCEPTION HANDLING required
     feed = feedparser.parse(FEEDS[publication])
+    return feed['entries']
 
-    # call to get weather data from API
-    weather = get_weather("London,UK")
 
-    # MY personal checks
-    for key in feed:
-        print('\nfeeds keys---\n', key)
+@app.route("/")
+def home():
+    # - get customized headlines, based on user input or default
+    # request.args grabs get requests, request.form grabs POST requests
+    publication = request.args.get("publication")  # the tag name given in the html form
+    if not publication:
+        publication = DEFAULTS['publication']
+    articulos = get_noticas(publication)
 
-    # first arg file, second arg **arguments
-    # remember .get() avoids not found mistakes
+    # get customized weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+
     return render_template("home.html",
                            publication=publication,
-                           articulos=feed['entries'],
+                           articulos=articulos,
                            weather=weather)
 
 
